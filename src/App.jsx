2657c0b54ce5bc5a5608064c72255a6d4c9a8390
vov62 <avivovgen1@gmail.com>
2023-11-06 13:28:58 +0200

    first inital

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./component/Header";
+import HomePage from "./pages/HomePage";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <HomePage />
+      </main>
+    </>
+  );
+}
+
+export default App;
