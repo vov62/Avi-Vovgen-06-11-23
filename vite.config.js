cec6752269ab838273da960e3d214de0410f93e5
vov62 <avivovgen1@gmail.com>
2023-11-13 10:12:29 +0200

    fix github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/Avi-Vovgen-07-11-23/",
+  plugins: [react()],
 });
